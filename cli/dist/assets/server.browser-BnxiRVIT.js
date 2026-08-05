a86a6facecb4c7c9088f4b509d82dff111a51e97
VanillaSky <hello@vanillasky.ai>
2026-08-05 18:16:15 +0200

    vanillasky skill v0.11.1

--- cli/dist/assets/server.browser-BnxiRVIT.js
+++ cli/dist/assets/server.browser-BnxiRVIT.js
@@ -1,4 +1,4 @@
-import{r as wr}from"./render-BEp6ALZB.js";var re={};/**
+import{r as wr}from"./render-D4aVGdVm.js";var re={};/**
  * @license React
  * react-dom-server-legacy.browser.production.min.js
  *
