bba20f97d47c7783ba9bd4b2357c4b9f1d978af8
VanillaSky <hello@vanillasky.ai>
2026-08-07 08:13:03 +0000

    vanillasky skill v0.25.0 (main 18089de)

--- cli/dist/assets/server.browser-BenFjJTl.js
+++ cli/dist/assets/server.browser-BenFjJTl.js
@@ -1,4 +1,4 @@
-import{r as wr}from"./render-d4Qd7Qqu.js";var re={};/**
+import{r as wr}from"./render-CPqYSKUz.js";var re={};/**
  * @license React
  * react-dom-server-legacy.browser.production.min.js
  *
